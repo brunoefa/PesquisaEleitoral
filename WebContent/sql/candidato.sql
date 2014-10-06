20a9834dc1fe453c5705cbd7efa7db1d5f514c5f
Bruno Eustáquio <brunoefa@gmail.com>
2014-10-06 19:31:31 -0300

    Script sql da tabela candidato

---
+++ WebContent/sql/candidato.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `candidato` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `foto` varchar(300) NOT NULL,
+  `partido` varchar(45) NOT NULL,
+  `cargo` varchar(45) NOT NULL,
+  `numero` varchar(5) NOT NULL,
+  `votos` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
